da8a1dcac0864304e4b024586e05e6f8d3f8b7f2
Washington Soares <washingtonudi@gmail.com>
2015-08-21 15:43:16 -0300

    first updates

--- js/anguvideo.js
+++ js/anguvideo.js
@@ -3,62 +3,98 @@
  */
 /*global angular*/
 angular.module('anguvideo', [])
-    .directive("anguvideo", ['$sce', function ($sce) {
-        return {
-            restrict: 'EA',
-            scope: {
-                source: '=ngModel',
-                width: '@',
-                height: '@'
-            },
-            replace: true,
-            template: '<div class="anguvideo">' +
-                            '<iframe class="videoClass" type="text/html" width="{{width}}" height="{{height}}" ng-src="{{url}}" allowfullscreen frameborder="0"></iframe>' +
-                      '</div>',
-            link: function (scope, element, attrs) {
-                var embedFriendlyUrl = "",
-                    urlSections,
-                    index;
-                
-                var youtubeParams = (attrs.hideControls ? '?autoplay=0&showinfo=0&controls=0' : '');
+  .directive("anguvideo", ['$sce','$rootScope', function ($sce,$rootScope) {
+      return {
+          restrict: 'EA',
+          scope: {
+              source: '=ngModel',
+              width: '@',
+              height: '@'
+          },
+          replace: true,
+          template: '<div class="anguvideo">' +
+                          '<iframe id="playerYV" class="videoClass" type="text/html" width="{{width}}" height="{{height}}" ng-src="{{url}}" allowfullscreen autoplay frameborder="0"></iframe>' +
+                    '</div>',
+          link: function (scope, element, attrs) {
 
-                scope.$watch('source', function (newVal) {
-                    if (newVal) {
-                        /*
-                        * Need to convert the urls into a friendly url that can be embedded and be used in the available online players the services have provided
-                        * for youtube: src="//www.youtube.com/embed/{{video_id}}"
-                        * for vimeo: src="http://player.vimeo.com/video/{{video_id}}
-                        */
-
-                        if (newVal.indexOf("vimeo") >= 0) { // Displaying a vimeo video
-                            if (newVal.indexOf("player.vimeo") >= 0) {
-                                embedFriendlyUrl = newVal;
-                            } else {
-                                embedFriendlyUrl = newVal.replace("https:", "http:");
-                                urlSections = embedFriendlyUrl.split(".com/");
-                                embedFriendlyUrl = embedFriendlyUrl.replace("vimeo", "player.vimeo");
-                                embedFriendlyUrl = embedFriendlyUrl.replace("/" + urlSections[urlSections.length - 1], "/video/" + urlSections[urlSections.length - 1] + "");
-                            }
-                        } else if (newVal.indexOf("youtu.be") >= 0) {
-
-                            index = newVal.indexOf(".be/");
-
-                            embedFriendlyUrl = newVal.slice(index + 4, newVal.length);
-                            embedFriendlyUrl = "http://www.youtube.com/embed/" + embedFriendlyUrl + youtubeParams;
-
-                        } else if (newVal.indexOf("youtube.com") >= 0) { // displaying a youtube video
-
-                            if (newVal.indexOf("embed") >= 0) {
-                                embedFriendlyUrl = newVal + youtubeParams;
-                            } else {
-                                embedFriendlyUrl = newVal.replace("/watch?v=", "/embed/") + youtubeParams;
-                            }
-                        }
-
-                        scope.url = $sce.trustAsResourceUrl(embedFriendlyUrl);
-                        //console.log("done",  scope.url, embedFriendlyUrl);
-                    }
-                });
+            var player;
+            function onPlayerStateChange(event) {
+              if (!event.data) {
+                $rootScope.$broadcast("anguvideo:finishVideo");
+              }
             }
-        };
-    }]);
+            (function onYouTubeIframeAPIReady() {
+              player = new YT.Player('playerYV', {
+                events: {
+                  'onStateChange': onPlayerStateChange
+                }
+              });
+            }());
+              var origin = location.origin;
+              var embedFriendlyUrl = "",
+                  urlSections,
+                  index;
+
+              var youtubeParams = (attrs.hideControls ? '?autoplay=0&showinfo=0&controls=0&enablejsapi=1&origin='+origin : '?autoplay=1&enablejsapi=1&origin='+origin);
+
+              scope.$watch('source', function (newVal) {
+                  if (newVal) {
+                      /*
+                      * Need to convert the urls into a friendly url that can be embedded and be used in the available online players the services have provided
+                      * for youtube: src="//www.youtube.com/embed/{{video_id}}"
+                      * for vimeo: src="http://player.vimeo.com/video/{{video_id}}
+                      */
+
+
+                      if (newVal.indexOf("vimeo") >= 0) { // Displaying a vimeo video
+                          if (newVal.indexOf("player.vimeo") >= 0) {
+                              embedFriendlyUrl = newVal;
+                          } else {
+                              embedFriendlyUrl = newVal.replace("https:", "http:");
+                              urlSections = embedFriendlyUrl.split(".com/");
+                              embedFriendlyUrl = embedFriendlyUrl.replace("vimeo", "player.vimeo");
+                              embedFriendlyUrl = embedFriendlyUrl.replace("/" + urlSections[urlSections.length - 1], "/video/" + urlSections[urlSections.length - 1] + "");
+                              embedFriendlyUrl += '?autoplay=1&api=1&player_id=playerYV';
+                          }
+
+                          $(function() {
+                                var iframe = $('#playerYV')[0];
+                                var player = $f(iframe);
+                                // When the player is ready, add listeners for pause, finish, and playProgress
+                                player.addEvent('ready', function() {
+                                    player.addEvent('finish', onFinish);
+                                });
+
+
+                                function onFinish(id) {
+                                    $rootScope.$broadcast("anguvideo:finishVideo");
+                                }
+                          });
+
+                      } else if (newVal.indexOf("youtu.be") >= 0) {
+
+                          index = newVal.indexOf(".be/");
+
+                          embedFriendlyUrl = newVal.slice(index + 4, newVal.length);
+                          embedFriendlyUrl = "http://www.youtube.com/embed/" + embedFriendlyUrl + youtubeParams;
+
+                      } else if (newVal.indexOf("youtube.com") >= 0) { // displaying a youtube video
+
+                          if (newVal.indexOf("embed") >= 0) {
+                              embedFriendlyUrl = newVal + youtubeParams;
+                          } else {
+                              embedFriendlyUrl = newVal.replace("/watch?v=", "/embed/") + youtubeParams;
+                          }
+
+                      }
+
+                      scope.url = $sce.trustAsResourceUrl(embedFriendlyUrl);
+                      //console.log("done",  scope.url, embedFriendlyUrl);
+
+
+
+                  }
+              });
+          }
+      };
+  }]);

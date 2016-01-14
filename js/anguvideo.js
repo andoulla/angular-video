cb41234eaf8debfe7212709b3fda0db4d9f8bb48
Alex Mateos <amateos@webpartners.es>
2016-01-14 12:14:33 +0100

    Fixed friendly urls for https and m.youtube.com

--- js/anguvideo.js
+++ js/anguvideo.js
@@ -13,28 +13,28 @@ angular.module('anguvideo', [])
             },
             replace: true,
             template: '<div class="anguvideo">' +
-                            '<iframe class="videoClass" type="text/html" width="{{width}}" height="{{height}}" ng-src="{{url}}" allowfullscreen frameborder="0"></iframe>' +
-                      '</div>',
+            '<iframe class="videoClass" type="text/html" width="{{width}}" height="{{height}}" ng-src="{{url}}" allowfullscreen frameborder="0"></iframe>' +
+            '</div>',
             link: function (scope, element, attrs) {
                 var embedFriendlyUrl = "",
                     urlSections,
                     index;
-                
+
                 var youtubeParams = (attrs.hideControls ? '?autoplay=0&showinfo=0&controls=0' : '');
 
                 scope.$watch('source', function (newVal) {
                     if (newVal) {
                         /*
-                        * Need to convert the urls into a friendly url that can be embedded and be used in the available online players the services have provided
-                        * for youtube: src="//www.youtube.com/embed/{{video_id}}"
-                        * for vimeo: src="http://player.vimeo.com/video/{{video_id}}
-                        */
+                         * Need to convert the urls into a friendly url that can be embedded and be used in the available online players the services have provided
+                         * for youtube: src="//www.youtube.com/embed/{{video_id}}"
+                         * for vimeo: src="http://player.vimeo.com/video/{{video_id}}
+                         */
 
                         if (newVal.indexOf("vimeo") >= 0) { // Displaying a vimeo video
                             if (newVal.indexOf("player.vimeo") >= 0) {
                                 embedFriendlyUrl = newVal;
                             } else {
-                                embedFriendlyUrl = newVal.replace("https:", "http:");
+                                embedFriendlyUrl = newVal.replace("http:", "https:");
                                 urlSections = embedFriendlyUrl.split(".com/");
                                 embedFriendlyUrl = embedFriendlyUrl.replace("vimeo", "player.vimeo");
                                 embedFriendlyUrl = embedFriendlyUrl.replace("/" + urlSections[urlSections.length - 1], "/video/" + urlSections[urlSections.length - 1] + "");
@@ -44,19 +44,19 @@ angular.module('anguvideo', [])
                             index = newVal.indexOf(".be/");
 
                             embedFriendlyUrl = newVal.slice(index + 4, newVal.length);
-                            embedFriendlyUrl = "http://www.youtube.com/embed/" + embedFriendlyUrl + youtubeParams;
-
+                            embedFriendlyUrl = "https://www.youtube.com/embed/" + embedFriendlyUrl + youtubeParams;
                         } else if (newVal.indexOf("youtube.com") >= 0) { // displaying a youtube video
-
                             if (newVal.indexOf("embed") >= 0) {
                                 embedFriendlyUrl = newVal + youtubeParams;
                             } else {
                                 embedFriendlyUrl = newVal.replace("/watch?v=", "/embed/") + youtubeParams;
+                                if (embedFriendlyUrl.indexOf('m.youtube.com') != -1) {
+                                    embedFriendlyUrl = embedFriendlyUrl.replace("m.youtube.com", "youtube.com");
+                                }
                             }
                         }
 
                         scope.url = $sce.trustAsResourceUrl(embedFriendlyUrl);
-                        //console.log("done",  scope.url, embedFriendlyUrl);
                     }
                 });
             }
